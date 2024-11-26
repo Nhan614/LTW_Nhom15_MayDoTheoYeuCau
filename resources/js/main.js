f8e32e908b206e9f86f363bd9d8a8725c401687d
Nhan614 <22130193@st.hcmuaf.edu.vn>
2024-11-26 23:16:05 +0700

    update admin

--- resources/js/main.js
+++ resources/js/main.js
@@ -23,32 +23,46 @@ $(document).ready( function(e){
   $('.admin-sidebar-1').click(function(e) {
     $('section').addClass('admin-hide')
     $('#admin-dashboard').removeClass('admin-hide')
-
+    $('.sidebar li').removeClass('sidebar-active')
+    $(this).addClass('sidebar-active')
   })
 
   $('.admin-sidebar-2').click(function(e) {
     $('section').addClass('admin-hide')
     $('#admin-products').removeClass('admin-hide')
+    $('.sidebar li').removeClass('sidebar-active')
+    $(this).addClass('sidebar-active')
   })
 
   $('.admin-sidebar-5').click(function(e) {
     $('section').addClass('admin-hide')
     $('#admin-user').removeClass('admin-hide')
+    $('.sidebar li').removeClass('sidebar-active')
+    $(this).addClass('sidebar-active')
   })
 
   $('.admin-sidebar-3').click(function(e) {
     $('section').addClass('admin-hide')
     $('#admin-orders').removeClass('admin-hide')
+    $('.sidebar li').removeClass('sidebar-active')
+    $(this).addClass('sidebar-active')
   })
-  
+  // onload sidebar
   if (window.location.hash === '#admin-dashboard') {
     $('#admin-dashboard').removeClass('admin-hide')
+    $('.admin-sidebar-1').addClass('sidebar-active')
   }
   if (window.location.hash === '#admin-products') {
     $('#admin-products').removeClass('admin-hide')
+    $('.admin-sidebar-2').addClass('sidebar-active')
   }
   if (window.location.hash === '#admin-user') {
     $('#admin-user').removeClass('admin-hide')
+    $('.admin-sidebar-5').addClass('sidebar-active')
+  }
+  if (window.location.hash === '#admin-orders') {
+    $('#admin-orders').removeClass('admin-hide')
+    $('.admin-sidebar-3').addClass('sidebar-active')
   }
 })
 
