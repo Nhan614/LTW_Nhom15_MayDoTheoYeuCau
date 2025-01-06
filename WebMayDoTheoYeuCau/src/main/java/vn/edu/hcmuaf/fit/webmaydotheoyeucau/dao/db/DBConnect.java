8893980e9cb83d42775433b410359280e2e77b50
Nhan614 <22130193@st.hcmuaf.edu.vn>
2025-01-06 15:25:06 +0700

    connect

---
+++ WebMayDoTheoYeuCau/src/main/java/vn/edu/hcmuaf/fit/webmaydotheoyeucau/dao/db/DBConnect.java
@@ -0,0 +1,30 @@
+package vn.edu.hcmuaf.fit.webmaydotheoyeucau.dao.db;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.jdbi.v3.core.Jdbi;
+
+import java.sql.SQLException;
+
+public class DBConnect {
+    private static Jdbi jdbi;
+
+    public static Jdbi get() {
+        if (jdbi == null) connect();
+        return jdbi;
+    }
+
+    public static void connect() {
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setURL("jdbc:mysql://" + DBProperties.host() + ":" + DBProperties.port() + "/" + DBProperties.dataName());
+        ds.setUser(DBProperties.userName());
+        ds.setPassword(DBProperties.password());
+        try {
+            ds.setAutoReconnect(true);
+            ds.setUseCompression(true);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        jdbi = Jdbi.create(ds);
+    }
+
+}
